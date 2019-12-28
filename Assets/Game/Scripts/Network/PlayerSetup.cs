1a4f750d8477caeac87c983ea87fe8d311c6b284
Bree Huynh <breehuynh@brees-imac.attlocal.net>
2019-12-28 10:51:36 -0800

    ep3

---
+++ Assets/Game/Scripts/Network/PlayerSetup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+    void Start()
+    {
+        if (!isLocalPlayer) {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        } else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null) 
+            {
+                Camera.main.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
