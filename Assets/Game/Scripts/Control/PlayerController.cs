6735963cb6bd7bfe6b5286a5830ef375f4053b54
Bree Huynh <breehuynh@brees-imac.attlocal.net>
2019-12-28 09:32:21 -0800

    added rotation, movement

--- Assets/Game/Scripts/Control/PlayerController.cs
+++ Assets/Game/Scripts/Control/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField]
     private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
 
     private PlayerMotor motor;
 
@@ -28,6 +30,21 @@ public class PlayerController : MonoBehaviour {
         // Apply movement
         motor.Move(_velocity);
 
+        // Calculate rotation as a 3D vector (turning around)
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        // Apply rotation
+        motor.Rotate(_rotation);
+
+        // Calculate camera rotation as a 3D vector (turning around)
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        // Apply rotation
+        motor.RotateCamera(_cameraRotation);
     }
     
 }
