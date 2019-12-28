e96d2abc3f56ae7c8b04e5b1e55b3bf3b5c0bc49
Bree Huynh <breehuynh@brees-imac.attlocal.net>
2019-12-27 22:11:50 -0800

    done w ep1

---
+++ Assets/Game/Scripts/Control/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        // Calculate movement velocity as a 3D vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov; 
+
+        // Final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        // Apply movement
+        motor.Move(_velocity);
+
+    }
+    
+}
---
+++ Assets/Game/Scripts/Control/PlayerMotor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+    public void Move(Vector3 velocity)
+    {
+
+    }
+}
