d0a7320737f2ad5efb77fec55dc75db3558b6c8f
Bree Huynh <breehuynh@Brees-iMac.local>
2019-12-28 21:25:46 -0800

    added game manager and damage system

---
+++ Assets/Game/Scripts/Combat/PlayerShoot.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    private const string PLAYER_TAG = "Player";
+
+    public PlayerWeapon weapon;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    void Start() 
+    {
+        if (cam == null)
+        {
+            Debug.LogError("PlayerShoot: No camera referenced!");
+            this.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    [Client]
+    void Shoot()
+    {
+        RaycastHit _hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        //Vector3 lookPosition;
+        if (Physics.Raycast(ray, out _hit, weapon.range, mask))
+        {
+            //lookPosition = _hit.point;
+            if (_hit.collider.tag == PLAYER_TAG)
+            {
+                CmdPlayerShot(_hit.collider.name, weapon.damage);
+            }
+        }
+    }
+
+    [Command]
+    void CmdPlayerShot(string _playerID, int _damage)
+    {
+        Debug.Log(_playerID + " has been shot.");
+
+        Player _player = GameManager.GetPlayer(_playerID);
+        _player.TakeDamage(_damage);
+    }
+}
--- Assets/Game/Scripts/Network/PlayerSetup.cs
+++ Assets/Game/Scripts/Network/PlayerSetup.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
 using Mirror;
 
+[RequireComponent(typeof(Player))]
 public class PlayerSetup : NetworkBehaviour
 {
     [SerializeField]
     Behaviour[] componentsToDisable;
 
+    [SerializeField]
+    string remoteLayerName = "RemotePlayer";
+
     Camera sceneCamera;
 
     void Start()
     {
-        if (!isLocalPlayer) {
-            for (int i = 0; i < componentsToDisable.Length; i++)
-            {
-                componentsToDisable[i].enabled = false;
-            }
+        if (!isLocalPlayer) 
+        {
+            DisableComponents();
+            AssignRemoteLayer();
         } else
         {
             sceneCamera = Camera.main;
@@ -25,11 +28,36 @@ public class PlayerSetup : NetworkBehaviour
         }
     }
 
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        string _netID = GetComponent<NetworkIdentity>().netId.ToString();
+        Player _player = GetComponent<Player>();
+
+        GameManager.RegisterPlayer(_netID, _player);
+    }
+
+    void AssignRemoteLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    void DisableComponents()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
+        }
+    }
+
     void OnDisable()
     {
         if (sceneCamera != null)
         {
             sceneCamera.gameObject.SetActive(true);
         }
+
+        GameManager.UnRegisterPlayer(transform.name);
     }
 }
